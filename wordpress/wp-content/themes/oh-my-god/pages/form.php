a51a84bd48361ee4114ce8fb5050c015c548bf72
Kamil Wilk <kamilwilk95@gmail.com>
2021-06-17 08:38:54 +0200

    add new template

---
+++ wordpress/wp-content/themes/oh-my-god/pages/form.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ * Template Name: Form template
+ * Template Post Type: post, page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+get_header();
+?>
+
+<div>TEST FORMA</div>
+
+<?php 
+get_footer();
+?>
